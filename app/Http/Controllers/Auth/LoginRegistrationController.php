379c967e919b572cc67e2c7618a203b86b88b74b
ep.proximasoft.net@gmail <ep.proximasoft.net@gmail.com>
2020-10-11 11:43:48 +0600

    redux add

--- app/Http/Controllers/Auth/LoginRegistrationController.php
+++ app/Http/Controllers/Auth/LoginRegistrationController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class LoginController extends Controller
+class LoginRegistrationController extends Controller
 {
     public function users()
     {
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Auth\LoginController;
+use \App\Http\Controllers\Auth\LoginRegistrationController;
 use App\Http\Controllers\User\PostController;
 use App\Http\Controllers\User\ProfileController;
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/registration', [LoginController::class, 'registration']);
-Route::get('/logout', [LoginController::class, 'logout']);
+Route::post('/login', [LoginRegistrationController::class, 'login']);
+Route::post('/registration', [LoginRegistrationController::class, 'registration']);
+Route::get('/logout', [LoginRegistrationController::class, 'logout']);
 
 Route::get('/posts/recent', [PostController::class, 'recentPosts']);
 Route::resource('/posts', PostController::class);
